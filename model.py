8b81f0e5da8964cff696b59ca7c1a347ca278ce2
D1ffic00lt <dm.filinov@gmail.com>
2024-06-28 18:20:38 +0700

    cv2 video capture tests

--- model.py
+++ model.py
@@ -1,18 +1,28 @@
-from enum import Enum
+import numpy as np
+import torch
+
+from ultralytics.utils.plotting import Annotator, colors
 from pathlib import Path
 
-import cv2
-import torch
-from ultralytics.utils.plotting import Annotator, colors
+try:
+    from yolov5.utils.augmentations import letterbox
+    from yolov5.models.common import DetectMultiBackend
+    from yolov5.utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadStreams, LoadScreenshots, LoadImages
+    from yolov5.utils.general import (check_file, increment_path, check_img_size, Profile,
+                                      non_max_suppression, scale_boxes, xyxy2xywh)
+    from yolov5.utils.torch_utils import select_device
+except ModuleNotFoundError:
+    import sys
 
-from yolov5.models.common import DetectMultiBackend
-from yolov5.utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadStreams, LoadScreenshots, LoadImages
-from yolov5.utils.general import (check_file, increment_path, check_img_size, Profile,
-                                  non_max_suppression, scale_boxes, xyxy2xywh)
-from yolov5.utils.torch_utils import select_device
+    sys.path.append("./yolov5/")
 
+    from yolov5.utils.augmentations import letterbox
+    from yolov5.models.common import DetectMultiBackend
+    from yolov5.utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadStreams, LoadScreenshots, LoadImages
+    from yolov5.utils.general import (check_file, increment_path, check_img_size, Profile,
+                                      non_max_suppression, scale_boxes, xyxy2xywh)
+    from yolov5.utils.torch_utils import select_device
 
-# sys.path.append('yolov5')
 
 class Output(object):
     XYXY: list[float]
@@ -35,13 +45,55 @@ class Output(object):
         return f"Output(\n\t{self.XYXY}, \n\t{self.XYWH}, \n\t{self.PROB}, \n\t{self.LABEL}, \n\t{self.FRAME.shape})"
 
 
+class Dataset(object):
+    def __init__(self, raw_frame):
+        self.raw_frame = raw_frame
+        self.mode = "image"
+        self.imgsz = (640, 640)
+        self.stride = 0
+
+    def __iter__(self):
+        im = letterbox(self.raw_frame, self.imgsz, stride=self.stride, auto=True)[0]  # padded resize
+        im = im.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+        im = np.ascontiguousarray(im)
+        return iter([("./result.jpg", im, im, "", "")])
+
+    def __len__(self):
+        return 1
+
+    def __next__(self):
+        im = letterbox(self.raw_frame, self.imgsz, stride=self.stride, auto=True)[0]  # padded resize
+        im = im.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+        im = np.ascontiguousarray(im)
+        return "./result.jpg", im, im, "", ""
+
+
 class Model(DetectMultiBackend):
     def __init__(self, weights, *args, **kwargs) -> None:
         super().__init__(*args, weights=weights, **kwargs)
 
+    @staticmethod
+    def intersection(user_box, true_box):
+        x, y, w, h = user_box
+        user_box = (x, y, x + w, y + h)
+
+        x, y, w, h = true_box
+        true_box = (x, y, x + w, y + h)
+
+        x1 = max(user_box[0], true_box[0])
+        y1 = max(user_box[1], true_box[1])
+        x2 = min(user_box[2], true_box[2])
+        y2 = min(user_box[3], true_box[3])
+
+        inter_area = max(0, x2 - x1 + 1) * max(0, y2 - y1 + 1)
+
+        return inter_area > 0
+
     def predict_one_sample(
             self,
-            source="data/images",  # file/dir/URL/glob/screen/0(webcam)
+            source="data/images",  # file/dir/URL/glob/screen/0(webcam),
+            use_tqdm: bool = False,  # TODO
+            scale: bool = False,
             return_frame=False,  # xyxy, xywh, frame
             imgsz=(640, 640),  # inference size (height, width)
             conf_thres=0.25,  # confidence threshold
@@ -60,7 +112,11 @@ class Model(DetectMultiBackend):
             exist_ok=False,  # existing project/name ok, do not increment
             hide_conf=False,  # hide confidences
             vid_stride=1,  # video frame-rate stride
-    ):
+    ) -> list[Output]:  # TODO: frames array
+        raw_frame = None
+        if isinstance(source, list | np.ndarray):
+            if isinstance(source[0], np.ndarray | list | int):
+                raw_frame = source.copy()
         source = str(source)
         save_img = not nosave and not source.endswith(".txt")  # save inference images
         is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
@@ -72,7 +128,7 @@ class Model(DetectMultiBackend):
 
         # Directories
         save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
-        (save_dir / "labels" if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+        # (save_dir / "labels" if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
 
         # Load model
         device = select_device(device)
@@ -81,13 +137,19 @@ class Model(DetectMultiBackend):
 
         # Dataloader
         bs = 1  # batch_size
-        if webcam:
-            dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
-            bs = len(dataset)
-        elif screenshot:
-            dataset = LoadScreenshots(source, img_size=imgsz, stride=stride, auto=pt)
+        if raw_frame is not None:
+            dataset = Dataset(raw_frame)
+            dataset.imgsz = imgsz
+            dataset.stride = stride
         else:
-            dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
+            if webcam:
+                dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
+                bs = len(dataset)
+            elif screenshot:
+                dataset = LoadScreenshots(source, img_size=imgsz, stride=stride, auto=pt)
+            else:
+                dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
+
         vid_path, vid_writer = [None] * bs, [None] * bs
 
         # Run inference
@@ -152,7 +214,7 @@ class Model(DetectMultiBackend):
                         confidence = float(conf)
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()
                         output = Output()
-                        output.XYXY = list(map(lambda x: x.item(),xyxy))
+                        output.XYXY = list(map(lambda x: x.item(), xyxy))
                         output.XYWH = xywh
                         output.LABEL = label
                         output.PROB = confidence
@@ -174,8 +236,7 @@ class Model(DetectMultiBackend):
                         outp.FRAME = im0.copy()
                 if save_img:
                     if dataset.mode == "image":
-                        # cv2.imwrite(save_path, im0)
-                        pass
+                        cv2.imwrite(save_path, im0)
                     else:
                         if vid_path[i] != save_path:
                             vid_path[i] = save_path
@@ -192,3 +253,33 @@ class Model(DetectMultiBackend):
                         vid_writer[i].write(im0)
             outputs.append(out)
         return outputs[0]
+
+
+if __name__ == '__main__':
+    import cv2
+
+    model = Model("./yolov5n.pt")
+    model.eval()
+
+    video = cv2.VideoCapture(0)
+    cv2.startWindowThread()
+    # cv2.namedWindow("Frame")
+    # cv2.namedWindow("BBFrame")
+    # frame = cv2.imread("test.jpg")
+    # cv2.imshow(f"Frame", frame)
+
+    cv2.waitKey(10000)
+    key = 0
+    while key != 27:
+        ret, frame = video.read()
+        if ret:
+            cv2.imshow("Frame", cv2.resize(frame, (300, 300)))
+            image = frame.copy()
+            preds = model.predict_one_sample(image, return_frame=True)
+            if len(preds) == 0:
+                key = cv2.waitKey(100)
+                continue
+
+        key = cv2.waitKey(100)
+    video.release()
+    cv2.destroyAllWindows()
